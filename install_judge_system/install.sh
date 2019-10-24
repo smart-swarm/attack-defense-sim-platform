05a68b892d1d57520e4863f0ca22de9d4191e03f
guozhenglong <guozhenglong.cn@gmail.com>
2019-10-25 00:21:39 +0800

    update the judge system software.

--- install_judge_system/install.sh
+++ install_judge_system/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "stat installing the judge system..."
+echo "start installing the judge system..."
 sudo dpkg -i ros-kinetic-judge-system-msgs_0.0.0-0xenial_amd64.deb
 sh load_custom_msgs_to_ros_env.sh
 sudo dpkg -i ros-kinetic-judge-system_0.0.0-0xenial_amd64.deb
