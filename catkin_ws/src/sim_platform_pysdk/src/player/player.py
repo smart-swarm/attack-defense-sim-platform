a7dad794bc67b23461509f337d99100f239e2e9c
guozhenglong <guozhenglong.cn@gmail.com>
2019-11-20 01:20:17 +0800

    support gui and mouse click to kill, reset ,ready and start fight.

--- catkin_ws/src/sim_platform_pysdk/src/player/player.py
+++ catkin_ws/src/sim_platform_pysdk/src/player/player.py
@@ -5,8 +5,10 @@ from example import Example
 if __name__ == "__main__":
     
     example = Example()
-    example.set_rate(30)
+    example.set_rate(50)
 
     while not example.is_shutdown():
         example.process()
         example.sleep()
+        
+        
